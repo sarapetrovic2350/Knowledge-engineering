5bf1eca243e98feac7700caff783148b269fa308
Sara Petrovic <petrovic.ra21.2019@uns.ac.rs>
2023-06-09 02:13:45 +0200

    Updated fuzzy service to get purpose type

--- owl.api.example/src/main/java/com/owl/api/example/controller/FuzzyController.java
+++ owl.api.example/src/main/java/com/owl/api/example/controller/FuzzyController.java
@@ -1,5 +1,6 @@
 package com.owl.api.example.controller;
 
+import com.owl.api.example.dto.PurposeDTO;
 import com.owl.api.example.service.FuzzyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +18,18 @@ public class FuzzyController {
         this.fuzzyService = fuzzyService;
     }
     @GetMapping("/purpose")
-    public ResponseEntity<Double> getPurposeType(
-            @RequestParam(required = true) double service,
-            @RequestParam(required = true) double food) {
-        return new ResponseEntity<>(this.fuzzyService.getPurposeType(service, food), HttpStatus.OK);
+    public ResponseEntity<PurposeDTO> getPurposeType(
+            @RequestParam(required = true) int cpu_cores,
+            @RequestParam(required = true) int cpu_threads,
+            @RequestParam(required = true) int cpu_tdp,
+            @RequestParam(required = true) int ram_capacity_in_gb,
+            @RequestParam(required = true) int ram_speed,
+            @RequestParam(required = true) double ram_voltage,
+            @RequestParam(required = true) int gpu_memory_in_gb,
+            @RequestParam(required = true) int gpu_clock_in_mhz,
+            @RequestParam(required = true) int psu_output_power,
+            @RequestParam(required = true) int hdd_capacity_in_gb) {
+        return new ResponseEntity<>(this.fuzzyService.getPurposeType(cpu_cores, cpu_threads, cpu_tdp, ram_capacity_in_gb, ram_speed,
+                ram_voltage, gpu_memory_in_gb, gpu_clock_in_mhz, psu_output_power, hdd_capacity_in_gb), HttpStatus.OK);
     }
 }
--- owl.api.example/src/main/java/com/owl/api/example/service/FuzzyService.java
+++ owl.api.example/src/main/java/com/owl/api/example/service/FuzzyService.java
@@ -1,5 +1,7 @@
 package com.owl.api.example.service;
 
+import com.owl.api.example.dto.MembershipDTO;
+import com.owl.api.example.dto.PurposeDTO;
 import net.sourceforge.jFuzzyLogic.FIS;
 import net.sourceforge.jFuzzyLogic.Gpr;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
@@ -15,30 +17,45 @@ public class FuzzyService {
             System.err.println("Can't load file: data/purposes.fcl");
         }
     }
-    public double getPurposeType(double service, double food){
+    public PurposeDTO getPurposeType(int cpu_cores, int cpu_threads, int cpu_tdp, int ram_capacity_in_gb,
+                                 int ram_speed, double ram_voltage, int gpu_memory_in_gb, int gpu_clock_in_mhz,
+                                 int psu_output_power, int hdd_capacity_in_gb) {
+        PurposeDTO purpose = new PurposeDTO();
+        MembershipDTO appDevelopmentMemberships = new MembershipDTO();
+        MembershipDTO gamingMemberships = new MembershipDTO();
 
-        //JFuzzyChart.get().chart(fis);
+        fis.setVariable("cpu_cores", cpu_cores);
+        fis.setVariable("cpu_threads", cpu_threads);
+        fis.setVariable("cpu_tdp", cpu_tdp);
+        fis.setVariable("ram_capacity_in_gb", ram_capacity_in_gb);
+        fis.setVariable("ram_speed", ram_speed);
+        fis.setVariable("ram_voltage", ram_voltage);
+        fis.setVariable("gpu_memory_in_gb", gpu_memory_in_gb);
+        fis.setVariable("gpu_clock_in_mhz", gpu_clock_in_mhz);
+        fis.setVariable("psu_output_power", psu_output_power);
+        fis.setVariable("hdd_capacity_in_gb", hdd_capacity_in_gb);
 
-        //fis.setVariable("service", service);
-        //fis.setVariable("food", food);
+        fis.evaluate();
+       // System.out.println(fis);
 
-        //fis.evaluate();
+        appDevelopmentMemberships.setPoor(fis.getVariable("app_development").getMembership("poor"));
+        appDevelopmentMemberships.setGood(fis.getVariable("app_development").getMembership("good"));
+        appDevelopmentMemberships.setExcellent(fis.getVariable("app_development").getMembership("excellent"));
+        purpose.setAppDevelopment(appDevelopmentMemberships);
 
-        //Variable tip = fis.getVariable("tip");
-        //JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
-        // Print ruleSet
-        System.out.println(fis);
-//        System.out.println("TIP: " + fis.getVariable("service").getMembership("excellent"));
-//        System.out.println("TIP: " + fis.getVariable("service").getMembership("good"));
-//        System.out.println("TIP: " + fis.getVariable("service").getMembership("poor"));
-//
-//        System.out.println("TIP: " + fis.getVariable("tip").getMembership("generous"));
-//        System.out.println("TIP: " + fis.getVariable("tip").getMembership("cheap"));
-//        System.out.println("TIP: " + fis.getVariable("tip").getMembership("average"));
-//        System.out.println("TIP: " + fis.getVariable("food").getMembership("delicious"));
-//        System.out.println("TIP: " + fis.getVariable("food").getMembership("rancid"));
-//
-//        return fis.getVariable("tip").getMembership("generous");
-        return 2.0;
+        gamingMemberships.setPoor(fis.getVariable("gaming").getMembership("poor"));
+        gamingMemberships.setGood(fis.getVariable("gaming").getMembership("good"));
+        gamingMemberships.setExcellent(fis.getVariable("gaming").getMembership("excellent"));
+        purpose.setGaming(gamingMemberships);
+
+        System.out.println("Cpu cores poor: " + fis.getVariable("cpu_cores").getMembership("poor"));
+        System.out.println("Cpu cores average: " + fis.getVariable("cpu_cores").getMembership("average"));
+        System.out.println("Cpu cores excellent: " + fis.getVariable("cpu_cores").getMembership("excellent"));
+
+        System.out.println("App dev purpose poor: " + fis.getVariable("app_development").getMembership("poor"));
+        System.out.println("App dev purpose good: " + fis.getVariable("app_development").getMembership("good"));
+        System.out.println("App dev purpose excellent: " + fis.getVariable("app_development").getMembership("excellent"));
+
+        return purpose;
     }
 }
